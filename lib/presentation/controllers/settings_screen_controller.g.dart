75d68e89f5bfc5e509da6c256a81c894427bed20
Francesco <chiccodallacinacolfurgone@yahoo.it>
2025-04-06 03:40:30 +0200

    fmt task

--- lib/presentation/controllers/settings_screen_controller.g.dart
+++ lib/presentation/controllers/settings_screen_controller.g.dart
@@ -12,17 +12,20 @@ String _$settingsScreenControllerHash() =>
 /// See also [SettingsScreenController].
 @ProviderFor(SettingsScreenController)
 final settingsScreenControllerProvider = AutoDisposeAsyncNotifierProvider<
-    SettingsScreenController, SettingsScreenState>.internal(
+  SettingsScreenController,
+  SettingsScreenState
+>.internal(
   SettingsScreenController.new,
   name: r'settingsScreenControllerProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$settingsScreenControllerHash,
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$settingsScreenControllerHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef _$SettingsScreenController
-    = AutoDisposeAsyncNotifier<SettingsScreenState>;
+typedef _$SettingsScreenController =
+    AutoDisposeAsyncNotifier<SettingsScreenState>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
--- lib/presentation/providers/theme_preferences_provider.g.dart
+++ lib/presentation/providers/theme_preferences_provider.g.dart
@@ -11,12 +11,15 @@ String _$themePreferencesHash() => r'8e2e144bbeb3d3c09f3f1dfc05013a36f5fb5b63';
 /// See also [ThemePreferences].
 @ProviderFor(ThemePreferences)
 final themePreferencesProvider = AutoDisposeAsyncNotifierProvider<
-    ThemePreferences, ThemePreferencesState>.internal(
+  ThemePreferences,
+  ThemePreferencesState
+>.internal(
   ThemePreferences.new,
   name: r'themePreferencesProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$themePreferencesHash,
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$themePreferencesHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
