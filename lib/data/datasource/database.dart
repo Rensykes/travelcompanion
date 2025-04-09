cfb1ef36f1bb4dec6caa9b8ebdea83f1e4343986
Francesco <chiccodallacinacolfurgone@yahoo.it>
2025-04-09 12:39:20 +0200

    added todo about db deletion

--- lib/data/datasource/database.dart
+++ lib/data/datasource/database.dart
@@ -6,6 +6,7 @@ import 'package:trackie/data/datasource/connection_helper.dart';
 
 part 'database.g.dart'; // Ensure this is correctly pointing to the generated file
 
+// TODO: https://github.com/simolus3/drift/issues/1470#issuecomment-2773180895 sqlite not removed always after the app is uninstalled
 @DriftDatabase(tables: [CountryVisits, LocationLogs, LogCountryRelations])
 class AppDatabase extends _$AppDatabase {
   AppDatabase() : super(openConnection());
@@ -15,17 +16,17 @@ class AppDatabase extends _$AppDatabase {
 
   @override
   MigrationStrategy get migration => MigrationStrategy(
-    // Run when the database is first created
-    onCreate: (Migrator m) async {
-      // Creating the table for log_country_relations
-      await m.createAll();
-    },
-    // Run when the database schema is upgraded (e.g., when the version is bumped)
-    onUpgrade: (Migrator m, int from, int to) async {
-      if (from < 2) {
-        // This is where you handle the logic to create missing tables or columns
-        await m.createTable(logCountryRelations);
-      }
-    },
-  );
+        // Run when the database is first created
+        onCreate: (Migrator m) async {
+          // Creating the table for log_country_relations
+          await m.createAll();
+        },
+        // Run when the database schema is upgraded (e.g., when the version is bumped)
+        onUpgrade: (Migrator m, int from, int to) async {
+          if (from < 2) {
+            // This is where you handle the logic to create missing tables or columns
+            await m.createTable(logCountryRelations);
+          }
+        },
+      );
 }
